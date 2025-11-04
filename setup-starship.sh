7af58c90e9d38614430547e8db5ee0624da03f28
adrianlanchares <adrianlopezlanchares@gmail.com>
2025-11-04 17:42:21 +0100

    fix typo

--- setup-starship.sh
+++ setup-starship.sh
@@ -3,7 +3,7 @@
 curl -sS https://starship.rs/install.sh | sh
 
 echo 'eval "$(starship init bash)"' >>~/.bashrc
-echo 'eval "$(starship init bash)"' >>~/home/coco/.bashrc
+echo 'eval "$(starship init bash)"' >>/home/coco/.bashrc
 
 cp starship.toml ~/.config/starship.toml
 cp starship.toml /home/coco/.config/starship.toml
