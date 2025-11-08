dfcaf8231c8cfcebba1bcb2ed9931ecf10928c2f
adrianlanchares <adrianlopezlanchares@gmail.com>
2025-11-08 21:31:44 +0100

    initial commit

---
+++ setup-tailscale.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Add tailscale package key and repository
+curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+
+# Install tailscale
+sudo apt update
+sudo apt install tailscale
+
+# Run tailscale
+sudo tailscale up
