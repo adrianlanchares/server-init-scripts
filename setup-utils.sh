8ed1cb940fdd2ed63a5ddca766aaf18c9e7cbb3c
adrianlanchares <adrianlopezlanchares@gmail.com>
2025-11-08 23:58:04 +0100

    initial commit

---
+++ setup-utils.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Install zoxide
+curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+
+echo "alias z='zoxide'" >>~/.bashrc
+
+# Install fzf
+sudo apt install fzf
+
+# Install lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/cagent
+echo "alias lzg='lazygit'" >>~/.bashrc
+
+# Install lazydocker
+curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
+echo "alias lzd='lazydocker'" >>~/.bashrc
