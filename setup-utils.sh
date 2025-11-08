ee7208a7ba6d46e4ad719970033ea17d03a54fac
adrianlanchares <adrianlopezlanchares@gmail.com>
2025-11-09 00:02:13 +0100

    Revert "try with apt"
    This reverts commit b0082d4dc7764623ae71b64a4ec197b34df2c028.

--- setup-utils.sh
+++ setup-utils.sh
@@ -9,9 +9,12 @@ echo "alias z='zoxide'" >>~/.bashrc
 sudo apt install fzf
 
 # Install lazygit
-sudo apt install lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/cagent
 echo "alias lzg='lazygit'" >>~/.bashrc
 
 # Install lazydocker
-sudo apt install lazydocker
+curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
 echo "alias lzd='lazydocker'" >>~/.bashrc
