b0082d4dc7764623ae71b64a4ec197b34df2c028
adrianlanchares <adrianlopezlanchares@gmail.com>
2025-11-09 00:01:39 +0100

    try with apt

--- setup-utils.sh
+++ setup-utils.sh
@@ -9,12 +9,9 @@ echo "alias z='zoxide'" >>~/.bashrc
 sudo apt install fzf
 
 # Install lazygit
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit.tar.gz lazygit
-sudo install lazygit -D -t /usr/local/bin/cagent
+sudo apt install lazygit
 echo "alias lzg='lazygit'" >>~/.bashrc
 
 # Install lazydocker
-curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
+sudo apt install lazydocker
 echo "alias lzd='lazydocker'" >>~/.bashrc
